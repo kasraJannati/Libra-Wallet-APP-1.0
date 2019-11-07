f74e95e4446eb4c9635563e6920d67d2f49aa7c4
Nrup R Patel <nruppatel133@gmail.com>
2019-11-07 00:31:42 -0500

    Update app.vue

--- src/app.vue
+++ src/app.vue
@@ -1,3 +1,7 @@
 <template>
   <f7-app :params="f7Params" class="page color-theme-blue">
     <f7-statusbar></f7-statusbar>
+    
+    <f7-panel left cover>
+      <f7-view url="/panel-left/" links-view=".view-main"></f7-view>
+    </f7-panel>
