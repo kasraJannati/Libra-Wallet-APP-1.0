2350819061ec45c85ad1025c8c38fc0140275f20
Nrup R Patel <nruppatel133@gmail.com>
2019-11-08 04:25:48 -0500

    Update app.vue

--- src/app.vue
+++ src/app.vue
@@ -5,3 +5,7 @@
     <f7-panel left cover>
       <f7-view url="/panel-left/" links-view=".view-main"></f7-view>
     </f7-panel>
+
+    <f7-panel right reveal>
+      <f7-view url="/panel-right/"></f7-view>
+    </f7-panel>
