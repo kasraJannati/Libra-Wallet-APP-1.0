6db2dbc453dfbd7a1dc06cf65bf0987aa28a5a96
Nrup R Patel <nruppatel133@gmail.com>
2019-11-08 04:32:28 -0500

    Update app-routes.js

--- src/app-routes.js
+++ src/app-routes.js
@@ -1,2 +1,14 @@
 import PageLibra           from '@/pages/page-libra.vue';
 import PageLibraAccount    from '@/pages/page-libra-account.vue';
+/*
+import PageAccountsTx     from '@/pages/page-accounts-tx.vue';
+import PageChat           from '@/pages/page-chat.vue';
+import PageExpenses       from '@/pages/page-expenses.vue';
+import PageHome           from '@/pages/page-home';
+import PageLogin          from '@/pages/page-login.vue';
+import PageTransfer       from '@/pages/page-transfer.vue';
+import PageWebpayBasic    from '@/pages/page-webpay-basic.vue';
+import PageWebpayTransfer from '@/pages/page-webpay-transfer.vue';
+import PanelLeft          from '@/pages/panel-left.vue';
+import PanelRight         from '@/pages/panel-right.vue';
+*/
