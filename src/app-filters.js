3755cf7f9d070b5a058867db43a4896f154141e0
Nrup R Patel <nruppatel133@gmail.com>
2019-11-09 01:10:13 -0500

    Update app-filters.js

--- src/app-filters.js
+++ src/app-filters.js
@@ -24,3 +24,8 @@ Vue.filter('formatCardDate', function (value) {
   if (value.length == 4) {
     return value.replace(/(\d{2})(\d{2})/, '$2/$1')
   }
+ const iso8601 = (new Date(+value)).toISOString();
+  const yy = iso8601.slice(2,4);
+  const mm = iso8601.slice(5,7);
+  return mm + '/' + yy;
+})
