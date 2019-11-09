101bf22da8a4808280632b12c38b7d7fe274aa17
Nrup R Patel <nruppatel133@gmail.com>
2019-11-09 01:06:15 -0500

    Update app-mixin.js

--- src/app-mixin.js
+++ src/app-mixin.js
@@ -1,3 +1,20 @@
 var preloaderCounter = 0;
 
 export default {
+/*
+  created (a,b) {
+    console.log('YOYO created root', this)
+  },*/
+
+  methods: {
+    //
+    setBackButtonHandler: function() {
+      /*self = this;
+      $(window).on('lifecycle', function (event) {
+        console.log('back-button')
+        self.$f7router.back();
+        if (event.detail.type == 'back-button') {
+          self.$f7router.back();
+        }
+      });*/
+    },
