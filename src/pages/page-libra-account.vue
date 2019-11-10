74695122ecee821bcb92701308256fe6615321eb
Nrup R Patel <nruppatel133@gmail.com>
2019-11-10 03:26:31 -0500

    Create page-libra-account.vue

---
+++ src/pages/page-libra-account.vue
@@ -0,0 +1,11 @@
+<template>
+  <f7-page name="page-libra-account" ptr @ptr:refresh="refreshOnPull">
+
+    <f7-navbar no-shadow color=white text-color="black">
+      <f7-nav-left>
+        <f7-link icon-f7="arrow_left" back></f7-link>
+      </f7-nav-left>
+      <f7-nav-title> Outgoing transactions </f7-nav-title>
+      <f7-nav-right>
+        <f7-link icon-if-md="material:send" @click="transfer()"/>
+      </f7-nav-right>
