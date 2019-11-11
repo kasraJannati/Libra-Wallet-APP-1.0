85b59103be732b24afe654a5ada44cf6a2025eef
Nrup R Patel <nruppatel133@gmail.com>
2019-11-11 01:00:13 -0500

    Update libra.js

--- src/libra/libra.js
+++ src/libra/libra.js
@@ -22,3 +22,32 @@ const accountCreate = async () => {
     'accountAddress': '234234234234234234234234',
   }*/
 }
+
+// List of accounts or an account for the given Id
+const accountList = async () => {
+  const resp = await axios.get(endpoint() + '/account/list')
+  return resp.data.accounts;
+  /*
+  return [
+    {
+      'index': '0',
+      'address'       : '0923784023840928340982340923',
+      'sequenceNumber': '0',
+      'status'        : 'Persisted',
+    },
+    {
+      'index': '1',
+      'address'       : '5675675675675678340982340923',
+      'sequencNumber' : '1',
+      'status'        : 'Local',
+    },
+  ]
+  */
+}
+
+//
+const accountMint = async (accountId, numCoins) => {
+  const resp = await axios.get(endpoint() + '/account/' + accountId + '/mint?amount=' + numCoins)
+  return resp.data;
+  //return {}
+}
